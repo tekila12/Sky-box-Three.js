fb208eb365f9d8158b21808fc71f0cb25a5571d9
Ajnur <radovic.ajnur96@gmail.com>
2022-12-05 06:11:53 +0100

    OrbitalControls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
+
+import { Canvas, useFrame, useThree } from '@react-three/fiber';
+import {
+  CubeTextureLoader,
+  CubeCamera,
+  WebGLCubeRenderTarget,
+  RGBFormat,
+  LinearMipmapLinearFilter
+} from "three";
 import './App.css';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+xtend({ OrbitControls });
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const CameraControls = () => {
+const {camera, gl :{domElement}}= useThree
+
+const controls= useRef()
+
+return(
+  <OrbitControls ref={controls} args={[camera, domElement]}
+  autoRotate={true} enable = {false}/>
+)
+}
+
+
+function SkyBox () {
+  const {scene} = useThree()
+  const loader = new CubeTextureLoader();
+
+  const texture = loader.load([
+    "/1.jpg",
+    "/2.jpg",
+    "/3.jpg",
+    "/4.jpg",
+    "/5.jpg",
+    "/6.jpg",
+  
+    
+ 
+  ]);
+
+  scene.background= texture;
+  return null
+}
+  function Sphere() {
+    const {scene, gl} = useThree()
+
+    const cubeRenderTarget = new WebGLCubeRenderTarget(256, {
+      format: RGBFormat,
+      generateMipmaps: true,
+      minFilter: LinearMipmapLinearFilter
+    });
+
+    const cubeCamera = new CubeCamera(1, 1000, cubeRenderTarget);
+    cubeCamera.position.set(0, 0, 0);
+    scene.add(cubeCamera);
+
+    useFrame(() => cubeCamera.update(gl, scene))
+    return (
+    <mesh>
+      <directionalLight intensity= {0.5} />
+      <sphereGeometry  attach= 'geometry' args={[ 2,32, 32]}/>
+      <meshBasicMaterial attach='material' color='orange' roughness={0.1} metalness={1} envMap={cubeCamera.renderTarget.texture}/>
+    </mesh>
+    )
+}
+return (
+   <Canvas>
+    <Sphere />
+    <SkyBox />
+   </Canvas>
+)
 }
 
 export default App;
